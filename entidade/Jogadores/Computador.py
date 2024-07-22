56dec52a79bdfd8153871f1bcc9994e0be085fe6
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-22 04:21:35 -0300

    descriçao para computador

--- entidade/Jogadores/Computador.py
+++ entidade/Jogadores/Computador.py
@@ -1,5 +1,13 @@
 from Jogador import Jogador
 
+'''No começo significava Allied Mastercomputer [Computador-mestre
+   Aliado], depois passou a significar Adaptative Manipulator 
+   [Manipulador Adaptativo], mas mais tarde ele se tornou consciente
+   e passaram a chamá-lo de Agressive Menace [Ameaça Agressiva], só 
+   que aí já era tarde demais e, por fim, ele mesmo, com sua 
+   inteligência emergente, passou a se chamar de AM, dizendo que isso
+   significa “I am”, ou seja, “eu sou”... I think, therefore I am... 
+   Penso, logo existo.'''
 
 class Computador(Jogador):
     def __init__(self):
